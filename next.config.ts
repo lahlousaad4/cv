68addf1b1c6e2de7ed5b8cf6ccee929ff4499f28
mohamedsaad_lahlou <mohamedsaad_lahlou@ens.univ-artois.fr>
2025-12-09 17:16:44 +0100

    erreur

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/cv",
-  assetPrefix: "/cv/",
-
   // Optimisations pour réduire la consommation énergétique
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
-
   // Désactiver les fonctionnalités non essentielles
   images: {
-    unoptimized: true, // DOIT être true pour export statique
+    unoptimized: false,
   },
-
   // Optimiser la compilation
   compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
